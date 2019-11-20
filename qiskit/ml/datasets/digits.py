23eb3a4a505fbf73b188e41a8765c9b5ade6b120
Manoel Marques <manoel@us.ibm.com>
2019-11-20 14:20:35 -0500

    Change AI to ML

---
+++ qiskit/ml/datasets/digits.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+digits dataset
+"""
+
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+try:
+    import matplotlib.pyplot as plt
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
+
+
+def digits(training_size, test_size, n, plot_data=False):
+    """ returns digits dataset """
+    class_labels = [r'A', r'B', r'C', r'D', r'E', r'F', r'G', r'H', r'I', r'J']
+    data = datasets.load_digits()
+    # pylint: disable=no-member
+    sample_train, sample_test, label_train, label_test = train_test_split(
+        data.data, data.target, test_size=0.3, random_state=22)
+
+    # Now we standardize for gaussian around 0 with unit variance
+    std_scale = StandardScaler().fit(sample_train)
+    sample_train = std_scale.transform(sample_train)
+    sample_test = std_scale.transform(sample_test)
+
+    # Now reduce number of features to number of qubits
+    pca = PCA(n_components=n).fit(sample_train)
+    sample_train = pca.transform(sample_train)
+    sample_test = pca.transform(sample_test)
+
+    # Scale to the range (-1,+1)
+    samples = np.append(sample_train, sample_test, axis=0)
+    minmax_scale = MinMaxScaler((-1, 1)).fit(samples)
+    sample_train = minmax_scale.transform(sample_train)
+    sample_test = minmax_scale.transform(sample_test)
+
+    # Pick training size number of samples from each distro
+    training_input = {key: (sample_train[label_train == k, :])[:training_size]
+                      for k, key in enumerate(class_labels)}
+    test_input = {key: (sample_test[label_test == k, :])[:test_size]
+                  for k, key in enumerate(class_labels)}
+
+    if plot_data:
+        if not HAS_MATPLOTLIB:
+            raise NameError('Matplotlib not installed. Plase install it before plotting')
+        for k in range(0, 9):
+            plt.scatter(sample_train[label_train == k, 0][:training_size],
+                        sample_train[label_train == k, 1][:training_size])
+
+        plt.title("PCA dim. reduced Digits dataset")
+        plt.show()
+
+    return sample_train, training_input, test_input, class_labels
