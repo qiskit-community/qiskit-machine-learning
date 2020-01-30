6335d11ad13da7da207f78a977d3f99f41399e46
Manoel Marques <manoel@us.ibm.com>
2020-01-30 17:33:10 -0500

    Add logging methods to all domains

--- qiskit/ml/__init__.py
+++ qiskit/ml/__init__.py
@@ -2,7 +2,7 @@
 
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2019.
+# (C) Copyright IBM 2019, 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -29,3 +29,9 @@ Submodules
    datasets
 
 """
+
+from ._logging import (get_qiskit_ml_logging,
+                       set_qiskit_ml_logging)
+
+__all__ = ['get_qiskit_ml_logging',
+           'set_qiskit_ml_logging']
