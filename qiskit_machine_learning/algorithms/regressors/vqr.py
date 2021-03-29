94440ecc170d96d3fcacbd8486fcf580549e50d9
beichensinn <beichen2@ie.ibm.com>
2021-03-29 23:47:01 +0100

    1. VQR updated with TwoLayerQNN 2. fit, predict and score functions were removed from VQR

--- qiskit_machine_learning/algorithms/regressors/vqr.py
+++ qiskit_machine_learning/algorithms/regressors/vqr.py
@@ -19,18 +19,19 @@ from qiskit.utils import QuantumInstance
 from qiskit.algorithms.optimizers import Optimizer
 
 from ...exceptions import QiskitMachineLearningError
-from ...neural_networks import CircuitQNN
+from ...neural_networks import TwoLayerQNN
 from ...utils.loss_functions.loss import Loss
 
 from .neural_network_regressor import NeuralNetworkRegressor
 
 
 class VQR(NeuralNetworkRegressor):
-    """Quantum neural network regressor."""
+    """Quantum neural network regressor using TwoLayerQNN"""
 
     def __init__(self,
                  feature_map: QuantumCircuit,
                  var_form: QuantumCircuit,
+                 observable: Union[QuantumCircuit, OperatorBase] = None,
                  loss: Union[str, Loss] = 'l2',
                  optimizer: Optimizer = None,
                  warm_start: bool = False,
@@ -39,6 +40,8 @@ class VQR(NeuralNetworkRegressor):
         Args:
             feature_map: The QuantumCircuit instance to use.
             var_form: The variational form instance.
+            observable: observable to be measured to determine the output of the network. If None
+                is given, the `Z^{\otimes num_qubits}` observable is used.
             loss: A target loss function to be used in training. Default is L2.
             optimizer: An instance of an optimizer to be used in training.
             warm_start: Use weights from previous fit to start next fit.
@@ -49,19 +52,18 @@ class VQR(NeuralNetworkRegressor):
         # construct circuit
         if feature_map.num_qubits != var_form.num_qubits:
             raise QiskitMachineLearningError('Feature map and var form need same number of qubits!')
+
         self._feature_map = feature_map
         self._var_form = var_form
         self._num_qubits = feature_map.num_qubits
-        self._circuit = QuantumCircuit(self._num_qubits)
-        self._circuit.compose(feature_map, inplace=True)
-        self._circuit.compose(var_form, inplace=True)
+        self._observable = observable
+
 
         # construct circuit QNN
-        neural_network = CircuitQNN(self._circuit,
-                                    feature_map.parameters,
-                                    var_form.parameters,
-                                    interpret=None,
-                                    output_shape=2,
+        neural_network = TwoLayerQNN(num_qubits=self._num_qubits,
+                                    feature_map=self.feature_map,
+                                    var_form=self.var_form,
+                                    obervable=self._observable,
                                     quantum_instance=quantum_instance)
 
         super().__init__(neural_network=neural_network,
@@ -80,59 +82,11 @@ class VQR(NeuralNetworkRegressor):
         return self._var_form
 
     @property
-    def circuit(self) -> QuantumCircuit:
-        """ Returns the underlying quantum circuit."""
-        return self._circuit
-
-    @property
     def num_qubits(self) -> int:
         """ Returns the number of qubits used by variational form and feature map."""
-        return self.circuit.num_qubits
+        return self._num_qubits
 
-    def fit(self, X: np.ndarray, y: np.ndarray):  # pylint: disable=invalid-name
-        """
-        Fit the model to data matrix X and target y.
 
-        Args:
-            X: The input data.
-            y: The target values.
 
-        Returns:
-            self: returns a trained regressor.
-        """
-        return super().fit(X, y)
 
-    def predict(self, X: np.ndarray) -> np.ndarray:  # pylint: disable=invalid-name
-        """
-        Predict using the network specified to the regression.
-
-        Args:
-            X: The input data.
-        Raises:
-            QiskitMachineLearningError: Model needs to be fit to some training data first
-        Returns:
-            The predicted values.
-        """
-        if self._fit_result is None:
-            raise QiskitMachineLearningError('Model needs to be fit to some training data first!')
-
-        return super().predict(X)
-
-    def score(self, X: np.ndarray, y: np.ndarray) -> int:  # pylint: disable=invalid-name
-        """
-        Return R-squared on the given test data and targeted values.
-
-        Args:
-            X: Test samples.
-            y: True target values given `X`.
-        Raises:
-            QiskitMachineLearningError: Model needs to be fit to some training data first
-        Returns:
-            R-squared value.
-        """
-
-        if self._fit_result is None:
-            raise QiskitMachineLearningError('Model needs to be fit to some training data first!')
-
-        return super().score(X, y)
 
