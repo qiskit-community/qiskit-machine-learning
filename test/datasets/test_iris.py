9f887ee77acc563e1956560fe6e0d5b56991f7c1
beichensinn <beichen2@ie.ibm.com>
2021-03-31 22:54:35 +0100

    test_iris is completed

--- test/datasets/test_iris.py
+++ test/datasets/test_iris.py
@@ -22,13 +22,14 @@ from qiskit_machine_learning.datasets import iris
 class TestIris(QiskitMachineLearningTestCase):
     """Iris tests."""
 
+
     def test_iris(self):
         """Iris test."""
 
-        input_file = self.get_resource_path('sample_train.iris',
-                                            'datasets')
-        with open(input_file) as file:
-            sample_train_ref = json.load(file)
+        # input_file = self.get_resource_path('sample_train.iris',
+        #                                     'datasets')
+        # with open(input_file) as file:
+        #     sample_train_ref = json.load(file)
 
         input_file = self.get_resource_path('training_input.iris',
                                             'datasets')
@@ -40,18 +41,22 @@ class TestIris(QiskitMachineLearningTestCase):
         with open(input_file) as file:
             test_input_ref = json.load(file)
 
-        sample_train, training_input, test_input, class_labels = iris(training_size=20,
-                                                                      test_size=10,
-                                                                      n=2,
-                                                                      plot_data=False)
-        np.testing.assert_allclose(sample_train.tolist(), sample_train_ref, rtol=1e-04)
-        for key, _ in training_input.items():
-            np.testing.assert_allclose(training_input[key].tolist(),
-                                       training_input_ref[key], rtol=1e-04)
-        for key, _ in test_input.items():
-            np.testing.assert_allclose(test_input[key].tolist(), test_input_ref[key], rtol=1e-04)
-        np.testing.assert_array_equal(class_labels, list(training_input.keys()))
+        training_features, _, test_features, test_labels = iris(training_size=20,
+                                                                test_size=3,
+                                                                n=2,
+                                                                plot_data=False)
+
+        training_features_ref = np.concatenate(list(training_input_ref.values()))
+        np.testing.assert_almost_equal(training_features_ref, training_features, 3)
+
+        test_features_ref = np.concatenate([x for x in list(test_input_ref.values()) if len(x) > 0])
+        np.testing.assert_almost_equal(test_features_ref, test_features, 3)
+        #
+        np.testing.assert_array_equal(test_labels.shape, (1, 3))
+
+        np.testing.assert_array_equal(np.sum(test_labels, axis=1), np.ones(1))
 
 
 if __name__ == '__main__':
     unittest.main()
+
