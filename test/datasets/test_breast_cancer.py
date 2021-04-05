1dc1014555e12b24084b56676d792b4422605da6
Anton Dekusar <adekusar@ie.ibm.com>
2021-04-05 14:28:17 +0100

    fix breast cancer

--- test/datasets/test_breast_cancer.py
+++ test/datasets/test_breast_cancer.py
@@ -25,15 +25,10 @@ class TestBreastCancer(QiskitMachineLearningTestCase):
     def test_breast_cancer(self):
         """Breast Cancer test."""
 
-        input_file = self.get_resource_path('training_input.breast_cancer',
+        input_file = self.get_resource_path('breast_cancer.ref',
                                             'datasets')
         with open(input_file) as file:
-            training_input_ref = json.load(file)
-
-        input_file = self.get_resource_path('test_input.breast_cancer',
-                                            'datasets')
-        with open(input_file) as file:
-            test_input_ref = json.load(file)
+            ref_data = json.load(file)
 
         training_features, _, test_features, test_labels = breast_cancer(
             training_size=20,
@@ -41,17 +36,11 @@ class TestBreastCancer(QiskitMachineLearningTestCase):
             n=2,
             plot_data=False)
 
-        training_features_ref = np.concatenate(list(training_input_ref.values()))
-        np.testing.assert_almost_equal(training_features_ref, training_features)
-
-        test_features_ref = np.concatenate(list(test_input_ref.values()))
-        np.testing.assert_almost_equal(test_features_ref, test_features)
+        np.testing.assert_almost_equal(ref_data["training_features"], training_features)
+        np.testing.assert_almost_equal(ref_data["test_features"], test_features)
 
         np.testing.assert_array_equal(test_labels.shape, (20, 2))
-        np.testing.assert_array_equal(test_labels,
-                                      [[1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0],
-                                       [1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1], [0, 1],
-                                       [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1]])
+        np.testing.assert_array_equal(ref_data["test_labels"],  test_labels)
 
 
 if __name__ == '__main__':
