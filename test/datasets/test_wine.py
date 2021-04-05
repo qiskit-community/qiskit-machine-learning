be0cd8957c6dc012c6817009481ca6a07c9ccdd0
Anton Dekusar <adekusar@ie.ibm.com>
2021-04-05 14:44:55 +0100

    fix wine

--- test/datasets/test_wine.py
+++ test/datasets/test_wine.py
@@ -25,29 +25,23 @@ class TestWine(QiskitMachineLearningTestCase):
     def test_wine(self):
         """Wine test."""
 
-        input_file = self.get_resource_path('training_input.wine',
+        input_file = self.get_resource_path('wine_ref.json',
                                             'datasets')
         with open(input_file) as file:
-            training_input_ref = json.load(file)
+            ref_data = json.load(file)
 
-        input_file = self.get_resource_path('test_input.wine',
-                                            'datasets')
-        with open(input_file) as file:
-            test_input_ref = json.load(file)
+        training_features, training_labels, test_features, test_labels = wine(
+            training_size=20,
+            test_size=10,
+            n=2,
+            plot_data=False,
+            one_hot=False)
 
-        training_features, _, test_features, test_labels = wine(training_size=20,
-                                                                test_size=10,
-                                                                n=2,
-                                                                plot_data=False,
-                                                                one_hot=False)
+        np.testing.assert_almost_equal(ref_data["training_features"], training_features)
+        np.testing.assert_almost_equal(ref_data["training_labels"], training_labels)
 
-        training_features_ref = np.concatenate(list(training_input_ref.values()))
-        np.testing.assert_almost_equal(training_features_ref, training_features)
-
-        test_features_ref = np.concatenate(list(test_input_ref.values()))
-        np.testing.assert_almost_equal(test_features_ref, test_features)
-
-        np.testing.assert_almost_equal(test_labels, [0, 0, 1, 1, 1, 2, 2, 2, 2, 2])
+        np.testing.assert_almost_equal(ref_data["test_features"], test_features)
+        np.testing.assert_almost_equal(ref_data["test_labels"], test_labels)
 
 
 if __name__ == '__main__':
