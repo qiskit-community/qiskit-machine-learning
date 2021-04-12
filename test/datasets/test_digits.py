7d9281522be610fa696a9eb2cf6e606a62f5219c
Manoel Marques <manoel.marques@ibm.com>
2021-04-12 14:00:49 -0400

    fix conflicts

--- test/datasets/test_digits.py
+++ test/datasets/test_digits.py
@@ -46,14 +46,9 @@ class TestDigits(QiskitMachineLearningTestCase):
         test_features_ref = np.concatenate(list(test_input_ref.values()))
         np.testing.assert_almost_equal(test_features_ref, test_features, 3)
 
-<<<<<<< HEAD
         np.testing.assert_array_equal(test_labels.shape, (100, 10))
         np.testing.assert_array_equal(np.sum(test_labels, axis=0), np.array([10] * 10))
         np.testing.assert_array_equal(np.sum(test_labels, axis=1), np.ones(100))
-=======
-        np.testing.assert_almost_equal(ref_data["test_features"], test_features, 3)
-        np.testing.assert_almost_equal(ref_data["test_labels"], test_labels, 4)
->>>>>>> eb2ac94... Disable Conan on CI Aer build (#45)
 
 
 if __name__ == '__main__':
