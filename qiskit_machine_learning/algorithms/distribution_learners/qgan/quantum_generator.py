Fix qGAN gradient behavior (#77)
* fix qgan grads

* include reno

* update test for ddt

* fix lint

* fix test

* fix lint

* fix lint

* delete test file

* fix lint again

* fix yaml

Co-authored-by: Manoel Marques <Manoel.Marques@ibm.com>
Co-authored-by: Anton Dekusar <62334182+adekusar-drl@users.noreply.github.com>

qiskit_machine_learning/algorithms/distribution_learners/qgan/quantum_generator.py
@@ -408,7 +408,8 @@ class QuantumGenerator(GenerativeNetwork):
             grad_object = gradient_object.convert(operator=op, params=free_params)
             value_dict = {free_params[i]: current_point[i] for i in range(len(free_params))}
             analytical_gradients = np.array(grad_object.assign_parameters(value_dict).eval())
-            loss_gradients = self.loss(prediction_generated, analytical_gradients).real
+            loss_gradients = self.loss(prediction_generated, np.transpose(
+                analytical_gradients)).real
             return loss_gradients
 
         return gradient_function