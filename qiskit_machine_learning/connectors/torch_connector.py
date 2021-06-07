Fixes wrong computation of gradients in TorchConnector when a batch of data is given as input. Changelog: Bugfix

qiskit_machine_learning/connectors/torch_connector.py
@@ -30,7 +30,7 @@ from ..exceptions import QiskitMachineLearningError
 logger = logging.getLogger(__name__)
 
 try:
-    from torch import Tensor, sparse_coo_tensor
+    from torch import Tensor, sparse_coo_tensor, einsum
     from torch.autograd import Function
     from torch.nn import Module, Parameter as TorchParam
 except ImportError:
@@ -150,6 +150,10 @@ class TorchConnector(Module):
                     + f" expected input compatible to {neural_network.num_inputs}"
                 )
 
+            # Ensure same shape for single observations and batch mode
+            if len(grad_output.shape) == 1:
+                grad_output = grad_output.view(1, -1)
+
             # evaluate QNN gradient
             input_grad, weights_grad = neural_network.backward(input_data.numpy(), weights.numpy())
             if input_grad is not None:
@@ -165,11 +169,7 @@ class TorchConnector(Module):
                     input_grad = input_grad.to(grad_output.dtype)
                 else:
                     input_grad = Tensor(input_grad).to(grad_output.dtype)
-
-                if len(grad_output.shape) == 2:
-                    input_grad = grad_output.transpose(0, 1) @ input_grad.transpose(0, 1)
-                else:
-                    input_grad = grad_output @ input_grad  # TODO: validate
+                input_grad = einsum("ij,ijk->k", grad_output, input_grad)
 
             if weights_grad is not None:
                 if np.prod(weights_grad.shape) == 0:
@@ -184,11 +184,7 @@ class TorchConnector(Module):
                     weights_grad = weights_grad.to(grad_output.dtype)
                 else:
                     weights_grad = Tensor(weights_grad).to(grad_output.dtype)
-
-                if len(grad_output.shape) == 2:
-                    weights_grad = grad_output.transpose(0, 1) @ weights_grad.transpose(0, 1)
-                else:
-                    weights_grad = grad_output @ weights_grad
+                weights_grad = einsum("ij,ijk->k", grad_output, weights_grad)
 
             # return gradients for the first two arguments and None for the others (i.e. qnn/sparse)
             return input_grad, weights_grad, None, None