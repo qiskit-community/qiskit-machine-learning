16e0ec41a3121b99c17a8d3cc1a7d05cc7659010
ElePT <epenatap@gmail.com>
2021-07-05 20:02:59 +0900

    Restore previous commit

--- qiskit_machine_learning/connectors/torch_connector.py
+++ qiskit_machine_learning/connectors/torch_connector.py
@@ -174,7 +174,7 @@ class TorchConnector(Module):
                 # from this point on backwards with respect to each input k. Sums over all i and
                 # j to get total gradient of output w.r.t. each input k.
                 input_grad = einsum("ij,ijk->k", grad_output, input_grad)
-                # fix for input_grad dimension mismatch
+                # fix for input_grad dimension mismatch for batch/non-batch
                 input_grad = input_grad.view(1, -1)
 
             if weights_grad is not None:
