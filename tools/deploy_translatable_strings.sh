86610e9c687110f15874bcbc8e48e6f488bef328
SooluThomas <soolu.elto@gmail.com>
2021-07-08 10:13:33 -0400

    Update deploy_translatable_strings.sh

--- tools/deploy_translatable_strings.sh
+++ tools/deploy_translatable_strings.sh
@@ -56,14 +56,17 @@ git config user.email "qiskit@qiskit.org"
 echo "git rm -rf for the translation po files"
 git rm -rf --ignore-unmatch machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/*.po \
     machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/api \
-    machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/apidoc \
+    machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/apidocs.po \
+    machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/stubs.po \
+    machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/release_notes.po \
     machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/theme \
     machine-learning/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/_*
 
 # Remove api/ and apidoc/ to avoid confusion while translating
 rm -rf $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/api/ \
-    $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/apidoc/ \
-    $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/stubs/ \
+    $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/apidocs.po \
+    $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/stubs.po \
+    $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/release_notes.po \
     $SOURCE_DIR/$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/theme/
 
 # Copy the new rendered files and add them to the commit.
