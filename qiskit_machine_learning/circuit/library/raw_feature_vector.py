88c672b27fcf38fc7416276488d532ba4a3c5121
ElePT <epenatap@gmail.com>
2021-08-10 10:44:43 +0900

    Add comment

--- qiskit_machine_learning/circuit/library/raw_feature_vector.py
+++ qiskit_machine_learning/circuit/library/raw_feature_vector.py
@@ -78,6 +78,8 @@ class RawFeatureVector(BlueprintCircuit):
 
     def _build(self):
         super()._build()
+        # we use len(_ordered_parameters) instead of feature_dimension
+        # to avoid checking if feature_dimension is None.
         num_qubits = np.log2(len(self._ordered_parameters[:]))
 
         if num_qubits != self.num_qubits:
