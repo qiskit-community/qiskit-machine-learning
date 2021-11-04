58f6219e6f75ed4063bf7fa123f3126e90d5b127
Arne-Thomsen <63008256+Arne-Thomsen@users.noreply.github.com>
2021-11-04 12:14:54 +0100

    Update qiskit_machine_learning/algorithms/classifiers/pegasos_qsvc.py
    Co-authored-by: Stefan Woerner <41292468+stefan-woerner@users.noreply.github.com>

--- qiskit_machine_learning/algorithms/classifiers/pegasos_qsvc.py
+++ qiskit_machine_learning/algorithms/classifiers/pegasos_qsvc.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Quantum Pegasos Support Vector Classifier."""
+"""Pegasos Quantum Support Vector Classifier."""
 
 import logging
 from datetime import datetime
