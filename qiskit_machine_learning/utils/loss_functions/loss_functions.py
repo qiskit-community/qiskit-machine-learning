859ad4f5003f6214abf28cb39338efd52ddab4c2
Caleb Johnson <calebj1524@outlook.com>
2021-11-10 17:48:45 -0600

    Clean up docstring for SVCLoss

--- qiskit_machine_learning/utils/loss_functions/loss_functions.py
+++ qiskit_machine_learning/utils/loss_functions/loss_functions.py
@@ -263,20 +263,23 @@ class CrossEntropySigmoidLoss(Loss):
 
 class SVCLoss(KernelLoss):
     r"""
-    This class provides a kernel loss function for classification tasks by fitting an ``SVC`` model
-    from sklearn. Given training samples x_{i} with binary labels y_{i} and a kernel K_{b}
-    parameterized by values b, the loss is defined as
-
     .. math::
 
-        SVCLoss = \sum_{i} a_i - 0.5 \sum_{i,j} a_i a_j y_{i} y_{j} K_b(x_i, x_j)
+        \text{This class provides a kernel loss function for classification tasks by
+        fitting an ``SVC`` model from sklearn. Given training samples, x_{i}, with binary
+        labels, y_{i}, and a kernel, K_{b}, parameterized by values b, the loss is defined as}
+    |
+    .. math::
+    SVCLoss = \sum_{i} a_i - 0.5 \sum_{i,j} a_i a_j y_{i} y_{j} K_b(x_i, x_j)
+    |
+    .. math::
+        \text{where a_i are the optimal Lagrange multipliers found by solving the standard
+        SVM quadratic program. Note that the hyperparameter C for the soft-margin penalty can
+        be specified through the keyword args.}
 
-    where a_i are the optimal Lagrange multipliers found by solving the standard SVM quadratic program.
-    Note that the hyper-parameter C for the soft-margin penalty can be specified through the keyword args.
-
-    Minimizing this loss over the parameters b of the kernel is equivalent to maximizing a weighted
-    kernel alignment, which in turn yields the smallest upper bound to the SVM generalization error for a
-    given set of parameter values.
+    Minimizing this loss over the parameters b of the kernel is equivalent to maximizing a
+    weighted kernel alignment, which in turn yields the smallest upper bound to the SVM
+    generalization error for a given parametrization.
 
     See https://arxiv.org/abs/2105.03406 for further details.
     """
