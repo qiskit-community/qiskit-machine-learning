73605f621d7f9cd266405aab114dd377796353ea
jonvet <vetterle@posteo.de>
2022-01-20 16:36:32 +0000

    Fix VQC training on multiclass datasets (#284)
    * fix parity function and add noise to CE loss for multiclass classification
    
    * add release notes and update year in copyright header to 2022
    
    * fix spelling
    
    * add docstring to test
    
    * add comments about how test dataset is created
    
    Co-authored-by: jonvet <jonas.vetterle.16@ucl.ac.uk>
    (cherry picked from commit 865e1878d0206f7fd5dcbccfe228181b97cc1cab)

--- test/algorithms/classifiers/test_vqc.py
+++ test/algorithms/classifiers/test_vqc.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2018, 2021.
+# (C) Copyright IBM 2018, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -153,6 +153,82 @@ class TestVQC(QiskitMachineLearningTestCase):
         # score
         score = classifier.score(X, y)
         self.assertGreater(score, 0.5)
+
+    @data(
+        # optimizer, quantum instance
+        ("cobyla", "statevector"),
+        ("cobyla", "qasm"),
+        ("bfgs", "statevector"),
+        ("bfgs", "qasm"),
+        (None, "statevector"),
+        (None, "qasm"),
+    )
+    def test_multiclass(self, config):
+        """Test multiclass VQC."""
+        opt, q_i = config
+
+        if q_i == "statevector":
+            quantum_instance = self.sv_quantum_instance
+        else:
+            quantum_instance = self.qasm_quantum_instance
+
+        if opt == "bfgs":
+            optimizer = L_BFGS_B(maxiter=5)
+        elif opt == "cobyla":
+            optimizer = COBYLA(maxiter=25)
+        else:
+            optimizer = None
+
+        num_inputs = 2
+        feature_map = ZZFeatureMap(num_inputs)
+        ansatz = RealAmplitudes(num_inputs, reps=1)
+        # fix the initial point
+        initial_point = np.array([0.5] * ansatz.num_parameters)
+
+        # construct classifier - note: CrossEntropy requires eval_probabilities=True!
+        classifier = VQC(
+            feature_map=feature_map,
+            ansatz=ansatz,
+            optimizer=optimizer,
+            quantum_instance=quantum_instance,
+            initial_point=initial_point,
+        )
+
+        # construct data
+        num_samples = 5
+        num_classes = 5
+        # pylint: disable=invalid-name
+
+        # We create a dataset that is random, but has some training signal, as follows:
+        # First, we create a random feature matrix X, but sort it by the row-wise sum in ascending
+        # order.
+        X = algorithm_globals.random.random((num_samples, num_inputs))
+        X = X[X.sum(1).argsort()]
+
+        # Next we create an array which contains all class labels, multiple times if num_samples <
+        # num_classes, and in ascending order (e.g. [0, 0, 1, 1, 2]). So now we have a dataset
+        # where the row-sum of X is correlated with the class label (i.e. smaller row-sum is more
+        # likely to belong to class 0, and big row-sum is more likely to belong to class >0)
+        y_indices = (
+            np.digitize(np.arange(0, 1, 1 / num_samples), np.arange(0, 1, 1 / num_classes)) - 1
+        )
+
+        # Third, we random shuffle both X and y_indices
+        permutation = np.random.permutation(np.arange(num_samples))
+        X = X[permutation]
+        y_indices = y_indices[permutation]
+
+        # Lastly we create a 1-hot label matrix y
+        y = np.zeros((num_samples, num_classes))
+        for e, index in enumerate(y_indices):
+            y[e, index] = 1
+
+        # fit to data
+        classifier.fit(X, y)
+
+        # score
+        score = classifier.score(X, y)
+        self.assertGreater(score, 1 / num_classes)
 
 
 if __name__ == "__main__":
