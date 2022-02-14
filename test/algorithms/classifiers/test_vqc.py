bf2f0d7d75aaeef61e55f8601b3699fc28c9cefb
Declan Millar <declan.millar@ibm.com>
2022-02-14 23:41:20 +0000

    uncomment other vqc tests

--- test/algorithms/classifiers/test_vqc.py
+++ test/algorithms/classifiers/test_vqc.py
@@ -47,252 +47,252 @@ class TestVQC(QiskitMachineLearningTestCase):
             seed_transpiler=algorithm_globals.random_seed,
         )
 
-    # @data(
-    #     # optimizer, quantum instance
-    #     ("cobyla", "statevector"),
-    #     ("cobyla", "qasm"),
-    #     ("bfgs", "statevector"),
-    #     ("bfgs", "qasm"),
-    #     (None, "statevector"),
-    #     (None, "qasm"),
-    # )
-    # def test_vqc(self, config):
-    #     """Test VQC."""
+    @data(
+        # optimizer, quantum instance
+        ("cobyla", "statevector"),
+        ("cobyla", "qasm"),
+        ("bfgs", "statevector"),
+        ("bfgs", "qasm"),
+        (None, "statevector"),
+        (None, "qasm"),
+    )
+    def test_vqc(self, config):
+        """Test VQC."""
 
-    #     opt, q_i = config
+        opt, q_i = config
 
-    #     if q_i == "statevector":
-    #         quantum_instance = self.sv_quantum_instance
-    #     else:
-    #         quantum_instance = self.qasm_quantum_instance
+        if q_i == "statevector":
+            quantum_instance = self.sv_quantum_instance
+        else:
+            quantum_instance = self.qasm_quantum_instance
 
-    #     if opt == "bfgs":
-    #         optimizer = L_BFGS_B(maxiter=5)
-    #     elif opt == "cobyla":
-    #         optimizer = COBYLA(maxiter=25)
-    #     else:
-    #         optimizer = None
+        if opt == "bfgs":
+            optimizer = L_BFGS_B(maxiter=5)
+        elif opt == "cobyla":
+            optimizer = COBYLA(maxiter=25)
+        else:
+            optimizer = None
 
-    #     num_inputs = 2
-    #     feature_map = ZZFeatureMap(num_inputs)
-    #     ansatz = RealAmplitudes(num_inputs, reps=1)
-    #     # fix the initial point
-    #     initial_point = np.array([0.5] * ansatz.num_parameters)
+        num_inputs = 2
+        feature_map = ZZFeatureMap(num_inputs)
+        ansatz = RealAmplitudes(num_inputs, reps=1)
+        # fix the initial point
+        initial_point = np.array([0.5] * ansatz.num_parameters)
 
-    #     # construct classifier - note: CrossEntropy requires eval_probabilities=True!
-    #     classifier = VQC(
-    #         feature_map=feature_map,
-    #         ansatz=ansatz,
-    #         optimizer=optimizer,
-    #         quantum_instance=quantum_instance,
-    #         initial_point=initial_point,
-    #     )
+        # construct classifier - note: CrossEntropy requires eval_probabilities=True!
+        classifier = VQC(
+            feature_map=feature_map,
+            ansatz=ansatz,
+            optimizer=optimizer,
+            quantum_instance=quantum_instance,
+            initial_point=initial_point,
+        )
 
-    #     # construct data
-    #     num_samples = 5
-    #     # pylint: disable=invalid-name
-    #     X = algorithm_globals.random.random((num_samples, num_inputs))
-    #     y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     while len(np.unique(y)) == 1:
-    #         X = algorithm_globals.random.random((num_samples, num_inputs))
-    #         y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input
+        # construct data
+        num_samples = 5
+        # pylint: disable=invalid-name
+        X = algorithm_globals.random.random((num_samples, num_inputs))
+        y = 1.0 * (np.sum(X, axis=1) <= 1)
+        while len(np.unique(y)) == 1:
+            X = algorithm_globals.random.random((num_samples, num_inputs))
+            y = 1.0 * (np.sum(X, axis=1) <= 1)
+        y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input
 
-    #     # fit to data
-    #     classifier.fit(X, y)
+        # fit to data
+        classifier.fit(X, y)
 
-    #     # score
-    #     score = classifier.score(X, y)
-    #     self.assertGreater(score, 0.5)
+        # score
+        score = classifier.score(X, y)
+        self.assertGreater(score, 0.5)
 
-    # @data(
-    #     # num_qubits, feature_map, ansatz
-    #     (True, False, False),
-    #     (True, True, False),
-    #     (True, True, True),
-    #     (False, True, True),
-    #     (False, False, True),
-    #     (True, False, True),
-    #     (False, True, False),
-    # )
-    # def test_default_parameters(self, config):
-    #     """Test VQC instantiation with default parameters."""
+    @data(
+        # num_qubits, feature_map, ansatz
+        (True, False, False),
+        (True, True, False),
+        (True, True, True),
+        (False, True, True),
+        (False, False, True),
+        (True, False, True),
+        (False, True, False),
+    )
+    def test_default_parameters(self, config):
+        """Test VQC instantiation with default parameters."""
 
-    #     provide_num_qubits, provide_feature_map, provide_ansatz = config
-    #     num_inputs = 2
+        provide_num_qubits, provide_feature_map, provide_ansatz = config
+        num_inputs = 2
 
-    #     num_qubits, feature_map, ansatz = None, None, None
+        num_qubits, feature_map, ansatz = None, None, None
 
-    #     if provide_num_qubits:
-    #         num_qubits = num_inputs
-    #     if provide_feature_map:
-    #         feature_map = ZZFeatureMap(num_inputs)
-    #     if provide_ansatz:
-    #         ansatz = RealAmplitudes(num_inputs, reps=1)
+        if provide_num_qubits:
+            num_qubits = num_inputs
+        if provide_feature_map:
+            feature_map = ZZFeatureMap(num_inputs)
+        if provide_ansatz:
+            ansatz = RealAmplitudes(num_inputs, reps=1)
 
-    #     classifier = VQC(
-    #         num_qubits=num_qubits,
-    #         feature_map=feature_map,
-    #         ansatz=ansatz,
-    #         quantum_instance=self.qasm_quantum_instance,
-    #     )
+        classifier = VQC(
+            num_qubits=num_qubits,
+            feature_map=feature_map,
+            ansatz=ansatz,
+            quantum_instance=self.qasm_quantum_instance,
+        )
 
-    #     # construct data
-    #     num_samples = 5
-    #     # pylint: disable=invalid-name
-    #     X = algorithm_globals.random.random((num_samples, num_inputs))
-    #     y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     while len(np.unique(y)) == 1:
-    #         X = algorithm_globals.random.random((num_samples, num_inputs))
-    #         y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input
+        # construct data
+        num_samples = 5
+        # pylint: disable=invalid-name
+        X = algorithm_globals.random.random((num_samples, num_inputs))
+        y = 1.0 * (np.sum(X, axis=1) <= 1)
+        while len(np.unique(y)) == 1:
+            X = algorithm_globals.random.random((num_samples, num_inputs))
+            y = 1.0 * (np.sum(X, axis=1) <= 1)
+        y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input
 
-    #     # fit to data
-    #     classifier.fit(X, y)
+        # fit to data
+        classifier.fit(X, y)
 
-    #     # score
-    #     score = classifier.score(X, y)
-    #     self.assertGreater(score, 0.5)
+        # score
+        score = classifier.score(X, y)
+        self.assertGreater(score, 0.5)
 
-    # @data(
-    #     # optimizer, quantum instance
-    #     ("cobyla", "statevector"),
-    #     ("cobyla", "qasm"),
-    #     ("bfgs", "statevector"),
-    #     ("bfgs", "qasm"),
-    #     (None, "statevector"),
-    #     (None, "qasm"),
-    # )
-    # def test_multiclass(self, config):
-    #     """Test multiclass VQC."""
-    #     opt, q_i = config
+    @data(
+        # optimizer, quantum instance
+        ("cobyla", "statevector"),
+        ("cobyla", "qasm"),
+        ("bfgs", "statevector"),
+        ("bfgs", "qasm"),
+        (None, "statevector"),
+        (None, "qasm"),
+    )
+    def test_multiclass(self, config):
+        """Test multiclass VQC."""
+        opt, q_i = config
 
-    #     if q_i == "statevector":
-    #         quantum_instance = self.sv_quantum_instance
-    #     else:
-    #         quantum_instance = self.qasm_quantum_instance
+        if q_i == "statevector":
+            quantum_instance = self.sv_quantum_instance
+        else:
+            quantum_instance = self.qasm_quantum_instance
 
-    #     if opt == "bfgs":
-    #         optimizer = L_BFGS_B(maxiter=5)
-    #     elif opt == "cobyla":
-    #         optimizer = COBYLA(maxiter=25)
-    #     else:
-    #         optimizer = None
+        if opt == "bfgs":
+            optimizer = L_BFGS_B(maxiter=5)
+        elif opt == "cobyla":
+            optimizer = COBYLA(maxiter=25)
+        else:
+            optimizer = None
 
-    #     num_inputs = 2
-    #     feature_map = ZZFeatureMap(num_inputs)
-    #     ansatz = RealAmplitudes(num_inputs, reps=1)
-    #     # fix the initial point
-    #     initial_point = np.array([0.5] * ansatz.num_parameters)
+        num_inputs = 2
+        feature_map = ZZFeatureMap(num_inputs)
+        ansatz = RealAmplitudes(num_inputs, reps=1)
+        # fix the initial point
+        initial_point = np.array([0.5] * ansatz.num_parameters)
 
-    #     # construct classifier - note: CrossEntropy requires eval_probabilities=True!
-    #     classifier = VQC(
-    #         feature_map=feature_map,
-    #         ansatz=ansatz,
-    #         optimizer=optimizer,
-    #         quantum_instance=quantum_instance,
-    #         initial_point=initial_point,
-    #     )
+        # construct classifier - note: CrossEntropy requires eval_probabilities=True!
+        classifier = VQC(
+            feature_map=feature_map,
+            ansatz=ansatz,
+            optimizer=optimizer,
+            quantum_instance=quantum_instance,
+            initial_point=initial_point,
+        )
 
-    #     # construct data
-    #     num_samples = 5
-    #     num_classes = 5
-    #     # pylint: disable=invalid-name
+        # construct data
+        num_samples = 5
+        num_classes = 5
+        # pylint: disable=invalid-name
 
-    #     # We create a dataset that is random, but has some training signal, as follows:
-    #     # First, we create a random feature matrix X, but sort it by the row-wise sum in ascending
-    #     # order.
-    #     X = algorithm_globals.random.random((num_samples, num_inputs))
-    #     X = X[X.sum(1).argsort()]
+        # We create a dataset that is random, but has some training signal, as follows:
+        # First, we create a random feature matrix X, but sort it by the row-wise sum in ascending
+        # order.
+        X = algorithm_globals.random.random((num_samples, num_inputs))
+        X = X[X.sum(1).argsort()]
 
-    #     # Next we create an array which contains all class labels, multiple times if num_samples <
-    #     # num_classes, and in ascending order (e.g. [0, 0, 1, 1, 2]). So now we have a dataset
-    #     # where the row-sum of X is correlated with the class label (i.e. smaller row-sum is more
-    #     # likely to belong to class 0, and big row-sum is more likely to belong to class >0)
-    #     y_indices = (
-    #         np.digitize(np.arange(0, 1, 1 / num_samples), np.arange(0, 1, 1 / num_classes)) - 1
-    #     )
+        # Next we create an array which contains all class labels, multiple times if num_samples <
+        # num_classes, and in ascending order (e.g. [0, 0, 1, 1, 2]). So now we have a dataset
+        # where the row-sum of X is correlated with the class label (i.e. smaller row-sum is more
+        # likely to belong to class 0, and big row-sum is more likely to belong to class >0)
+        y_indices = (
+            np.digitize(np.arange(0, 1, 1 / num_samples), np.arange(0, 1, 1 / num_classes)) - 1
+        )
 
-    #     # Third, we random shuffle both X and y_indices
-    #     permutation = np.random.permutation(np.arange(num_samples))
-    #     X = X[permutation]
-    #     y_indices = y_indices[permutation]
+        # Third, we random shuffle both X and y_indices
+        permutation = np.random.permutation(np.arange(num_samples))
+        X = X[permutation]
+        y_indices = y_indices[permutation]
 
-    #     # Lastly we create a 1-hot label matrix y
-    #     y = np.zeros((num_samples, num_classes))
-    #     for e, index in enumerate(y_indices):
-    #         y[e, index] = 1
+        # Lastly we create a 1-hot label matrix y
+        y = np.zeros((num_samples, num_classes))
+        for e, index in enumerate(y_indices):
+            y[e, index] = 1
 
-    #     # fit to data
-    #     classifier.fit(X, y)
+        # fit to data
+        classifier.fit(X, y)
 
-    #     # score
-    #     score = classifier.score(X, y)
-    #     self.assertGreater(score, 1 / num_classes)
+        # score
+        score = classifier.score(X, y)
+        self.assertGreater(score, 1 / num_classes)
 
-    # @data(
-    #     # optimizer, quantum instance
-    #     ("cobyla", "statevector"),
-    #     ("cobyla", "qasm"),
-    #     ("bfgs", "statevector"),
-    #     ("bfgs", "qasm"),
-    #     (None, "statevector"),
-    #     (None, "qasm"),
-    # )
-    # def test_warm_start(self, config):
-    #     """Test VQC with warm_start=True."""
-    #     opt, q_i = config
+    @data(
+        # optimizer, quantum instance
+        ("cobyla", "statevector"),
+        ("cobyla", "qasm"),
+        ("bfgs", "statevector"),
+        ("bfgs", "qasm"),
+        (None, "statevector"),
+        (None, "qasm"),
+    )
+    def test_warm_start(self, config):
+        """Test VQC with warm_start=True."""
+        opt, q_i = config
 
-    #     if q_i == "statevector":
-    #         quantum_instance = self.sv_quantum_instance
-    #     else:
-    #         quantum_instance = self.qasm_quantum_instance
+        if q_i == "statevector":
+            quantum_instance = self.sv_quantum_instance
+        else:
+            quantum_instance = self.qasm_quantum_instance
 
-    #     if opt == "bfgs":
-    #         optimizer = L_BFGS_B(maxiter=5)
-    #     elif opt == "cobyla":
-    #         optimizer = COBYLA(maxiter=25)
-    #     else:
-    #         optimizer = None
+        if opt == "bfgs":
+            optimizer = L_BFGS_B(maxiter=5)
+        elif opt == "cobyla":
+            optimizer = COBYLA(maxiter=25)
+        else:
+            optimizer = None
 
-    #     num_inputs = 2
-    #     feature_map = ZZFeatureMap(num_inputs)
-    #     ansatz = RealAmplitudes(num_inputs, reps=1)
+        num_inputs = 2
+        feature_map = ZZFeatureMap(num_inputs)
+        ansatz = RealAmplitudes(num_inputs, reps=1)
 
-    #     # Construct the data.
-    #     num_samples = 10
-    #     # pylint: disable=invalid-name
-    #     X = algorithm_globals.random.random((num_samples, num_inputs))
-    #     y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     while len(np.unique(y)) == 1:
-    #         X = algorithm_globals.random.random((num_samples, num_inputs))
-    #         y = 1.0 * (np.sum(X, axis=1) <= 1)
-    #     y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input.
+        # Construct the data.
+        num_samples = 10
+        # pylint: disable=invalid-name
+        X = algorithm_globals.random.random((num_samples, num_inputs))
+        y = 1.0 * (np.sum(X, axis=1) <= 1)
+        while len(np.unique(y)) == 1:
+            X = algorithm_globals.random.random((num_samples, num_inputs))
+            y = 1.0 * (np.sum(X, axis=1) <= 1)
+        y = np.array([y, 1 - y]).transpose()  # VQC requires one-hot encoded input.
 
-    #     # Initialize the VQC.
-    #     classifier = VQC(
-    #         feature_map=feature_map,
-    #         ansatz=ansatz,
-    #         optimizer=optimizer,
-    #         warm_start=True,
-    #         quantum_instance=quantum_instance,
-    #     )
+        # Initialize the VQC.
+        classifier = VQC(
+            feature_map=feature_map,
+            ansatz=ansatz,
+            optimizer=optimizer,
+            warm_start=True,
+            quantum_instance=quantum_instance,
+        )
 
-    #     # Fit the VQC to the first half of the data.
-    #     num_start = num_samples // 2
-    #     classifier.fit(X[:num_start, :], y[:num_start])
-    #     first_fit_final_point = classifier._fit_result.x
+        # Fit the VQC to the first half of the data.
+        num_start = num_samples // 2
+        classifier.fit(X[:num_start, :], y[:num_start])
+        first_fit_final_point = classifier._fit_result.x
 
-    #     # Fit the VQC to the second half of the data with a warm start.
-    #     classifier.fit(X[num_start:, :], y[num_start:])
-    #     second_fit_initial_point = classifier._initial_point
+        # Fit the VQC to the second half of the data with a warm start.
+        classifier.fit(X[num_start:, :], y[num_start:])
+        second_fit_initial_point = classifier._initial_point
 
-    #     # Check the final optimization point from the first fit was used to start the second fit.
-    #     np.testing.assert_allclose(first_fit_final_point, second_fit_initial_point)
+        # Check the final optimization point from the first fit was used to start the second fit.
+        np.testing.assert_allclose(first_fit_final_point, second_fit_initial_point)
 
-    #     # Check score.
-    #     score = classifier.score(X, y)
-    #     self.assertGreater(score, 0.5)
+        # Check score.
+        score = classifier.score(X, y)
+        self.assertGreater(score, 0.5)
 
     @data(
         # optimizer, quantum instance
