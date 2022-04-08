5497f9f36f41103b87fa11f37542efefec7d353d
Anton Dekusar <adekusar@ie.ibm.com>
2022-04-08 13:22:37 +0100

    explicit None check

--- qiskit_machine_learning/algorithms/trainable_model.py
+++ qiskit_machine_learning/algorithms/trainable_model.py
@@ -91,7 +91,9 @@ class TrainableModel(SerializableModelMixin):
             else:
                 raise QiskitMachineLearningError(f"Unknown loss {loss}!")
 
-        self._optimizer = optimizer or SLSQP()
+        # call the setter that has some additional checks
+        self.optimizer = optimizer
+
         self._warm_start = warm_start
         self._fit_result = None
         self._initial_point = initial_point
@@ -115,7 +117,9 @@ class TrainableModel(SerializableModelMixin):
     @optimizer.setter
     def optimizer(self, optimizer: Optional[Optimizer] = None):
         """Sets the optimizer to use in training process."""
-        self._optimizer = optimizer or SLSQP()
+        if optimizer is None:
+            optimizer = SLSQP()
+        self._optimizer = optimizer
 
     @property
     def warm_start(self) -> bool:
