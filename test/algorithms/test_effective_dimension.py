1933f7426afbb8069bd6bc0c01cb6a48d460cce1
ElePT <epenatap@gmail.com>
2022-04-08 11:19:24 -0400

    Modify random seed

--- test/algorithms/test_effective_dimension.py
+++ test/algorithms/test_effective_dimension.py
@@ -39,7 +39,7 @@ class TestEffDim(QiskitMachineLearningTestCase):
         super().setUp()
 
         # fix seeds
-        algorithm_globals.random_seed = 12345
+        algorithm_globals.random_seed = 1234
         qi_sv = QuantumInstance(
             Aer.get_backend("aer_simulator_statevector"),
             seed_simulator=algorithm_globals.random_seed,
@@ -93,10 +93,10 @@ class TestEffDim(QiskitMachineLearningTestCase):
         self.n = 5000
 
         # define results
-        self.result1 = 4.62355185  # 3,10,10
+        self.result1 = 4.68447003  # 3,10,10
         self.result2 = 1.39529449  # 3,1,1
-        self.result3 = 4.92825035  # 3,10,1
-        self.result4 = 5.93064172  # circuitqnn2 with 3,10,10
+        self.result3 = 4.62026643 # 3,10,1
+        self.result4 = 5.88157388  # circuitqnn2 with 3,10,10
 
     @data(
         # num_inputs, num_params
@@ -123,7 +123,7 @@ class TestEffDim(QiskitMachineLearningTestCase):
                 result = self.result3
 
         global_ed = EffectiveDimension(
-            qnn=qnn, num_params=num_params, num_inputs=num_inputs, fix_seed=True
+            qnn=qnn, num_params=num_params, num_inputs=num_inputs,
         )
 
         effdim = global_ed.get_eff_dim(self.n)
@@ -138,11 +138,11 @@ class TestEffDim(QiskitMachineLearningTestCase):
         qnn2 = self.opflow_qnn
 
         global_ed1 = EffectiveDimension(
-            qnn=qnn1, num_params=num_params, num_inputs=num_inputs, fix_seed=True
+            qnn=qnn1, num_params=num_params, num_inputs=num_inputs,
         )
 
         global_ed2 = EffectiveDimension(
-            qnn=qnn2, num_params=num_params, num_inputs=num_inputs, fix_seed=True
+            qnn=qnn2, num_params=num_params, num_inputs=num_inputs,
         )
 
         effdim1 = global_ed1.get_eff_dim(self.n)
@@ -152,25 +152,26 @@ class TestEffDim(QiskitMachineLearningTestCase):
 
     def test_custom_data(self):
         """Test that the results are equivalent for equal custom and generated data."""
+
         num_inputs, num_params = 10, 10
         qnn = self.circuit_qnn_1
-
         np.random.seed(0)
-        inputs = np.random.normal(0, 1, size=(num_inputs, qnn.num_inputs))
-        np.random.seed(0)
-        params = np.random.uniform(0, 1, size=(num_params, qnn.num_weights))
+        inputs = np.random.normal(0, 1, size=(10, qnn.num_inputs))
+        np.random.seed(0) # if seed not set again, test fails
+        params = np.random.uniform(0, 1, size=(10, qnn.num_weights))
 
         global_ed1 = EffectiveDimension(
-            qnn=qnn, num_params=num_params, num_inputs=num_inputs, fix_seed=True
+            qnn=qnn, num_params=num_params, num_inputs=num_inputs, seed = 0
         )
 
-        global_ed2 = EffectiveDimension(qnn=qnn, params=params, inputs=inputs, fix_seed=True)
+        global_ed2 = EffectiveDimension(qnn=qnn, params=params, inputs=inputs, seed = 0)
 
         effdim1 = global_ed1.get_eff_dim(self.n)
         effdim2 = global_ed2.get_eff_dim(self.n)
 
-        self.assertTrue(global_ed1._inputs.all() == global_ed2._inputs.all())
-        self.assertTrue(global_ed1._params.all() == global_ed2._params.all())
+        np.testing.assert_array_equal(global_ed1._inputs, global_ed2._inputs)
+        np.testing.assert_array_equal(global_ed1._params, global_ed2._params)
+
         self.assertAlmostEqual(effdim1, effdim2, 5)
 
     def test_multiple_samples(self):
@@ -180,13 +181,13 @@ class TestEffDim(QiskitMachineLearningTestCase):
         qnn = self.circuit_qnn_1
 
         global_ed1 = EffectiveDimension(
-            qnn=qnn, num_params=num_params, num_inputs=num_inputs, fix_seed=True
+            qnn=qnn, num_params=num_params, num_inputs=num_inputs,
         )
 
         effdim1 = global_ed1.get_eff_dim(self.n_list)
         effdim2 = global_ed1.get_eff_dim(np.asarray(self.n_list))
 
-        self.assertTrue(effdim1.all() == effdim2.all())
+        np.testing.assert_array_equal(effdim1,  effdim2)
 
     def test_local_ed_error(self):
         """Test that QiskitMachineLearningError is raised for wrong use
@@ -195,13 +196,11 @@ class TestEffDim(QiskitMachineLearningTestCase):
         with self.assertRaises(QiskitMachineLearningError):
 
             qnn = self.circuit_qnn_1
-            np.random.seed(0)
-            inputs = np.random.normal(0, 1, size=(10, qnn.num_inputs))
-            np.random.seed(0)
-            params = np.random.uniform(0, 1, size=(10, qnn.num_weights))
+            inputs = algorithm_globals.random.normal(0, 1, size=(10, qnn.num_inputs))
+            params = algorithm_globals.random.uniform(0, 1, size=(10, qnn.num_weights))
 
             local_ed1 = LocalEffectiveDimension(
-                qnn=qnn, inputs=inputs, params=params, fix_seed=True
+                qnn=qnn, inputs=inputs, params=params,
             )
             local_ed1.get_eff_dim(self.n)
 
