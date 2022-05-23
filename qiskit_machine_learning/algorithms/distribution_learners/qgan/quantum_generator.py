Fix qGAN custom gradients

qiskit_machine_learning/algorithms/distribution_learners/qgan/quantum_generator.py
@@ -23,7 +23,7 @@ from qiskit.circuit.library import TwoLocal
 from qiskit.utils import algorithm_globals, QuantumInstance
 from qiskit.algorithms.optimizers import ADAM, Optimizer
 from qiskit.opflow.gradients import Gradient
-from qiskit.opflow import CircuitStateFn
+from qiskit.opflow import CircuitStateFn, CircuitSampler
 from ....exceptions import QiskitMachineLearningError
 from .generative_network import GenerativeNetwork
 from .discriminative_network import DiscriminativeNetwork
@@ -306,14 +306,17 @@ class QuantumGenerator(GenerativeNetwork):
             result = result.get_statevector(qc)
             values = np.multiply(result, np.conj(result))
             values = list(values.real)
-            keys = []
-            for j in range(len(values)):
-                keys.append(np.binary_repr(j, int(sum(self._num_qubits))))
         else:
             result = result.get_counts(qc)
-            keys = list(result)
-            values = list(result.values())
-            values = [float(v) / np.sum(values) for v in values]
+            values_temp = list(result.values())
+            keys_temp = list(result)
+            values = np.zeros(2 ** int(sum(self._num_qubits)))
+            for i, key in enumerate(keys_temp):
+                values[int(key, 2)] = values_temp[i] / np.sum(values_temp)
+        keys = np.vectorize(np.binary_repr)(
+            np.arange(0, 2 ** int(sum(self._num_qubits))), width=int(sum(self._num_qubits))
+        )
+        # TODO In order to ensure scalability, the above line needs to be refactored into a sparse format.
         generated_samples_weights = values
         for i, _ in enumerate(keys):
             index = 0
@@ -417,10 +420,23 @@ class QuantumGenerator(GenerativeNetwork):
                 quantum_instance, params=current_point, shots=self._shots
             )
             prediction_generated = discriminator.get_label(generated_data, detach=True)
-            op = ~CircuitStateFn(primitive=self.generator_circuit)
+            op = CircuitStateFn(primitive=self.generator_circuit)
             grad_object = gradient_object.convert(operator=op, params=free_params)
             value_dict = {free_params[i]: current_point[i] for i in range(len(free_params))}
-            analytical_gradients = np.array(grad_object.assign_parameters(value_dict).eval())
+            if quantum_instance.backend is not None:
+                grad_object = (
+                    CircuitSampler(quantum_instance.backend).convert(grad_object, value_dict).eval()
+                )
+                if quantum_instance.is_statevector:
+                    analytical_gradients = np.array(grad_object)
+                else:
+                    analytical_gradients = np.zeros((len(grad_object), grad_object[0].shape[1]))
+                    for i in range(len(grad_object)):
+                        for j in range(grad_object[0].shape[1]):
+                            analytical_gradients[i, j] = grad_object[i][0, j].real
+
+            else:
+                analytical_gradients = np.array(grad_object.assign_parameters(value_dict).eval())
             loss_gradients = self.loss(
                 prediction_generated, np.transpose(analytical_gradients)
             ).real