2bb04c889a405976a2699b5ff371500b89c1464d
Anton Dekusar <adekusar@ie.ibm.com>
2023-03-20 09:58:58 +0000

    rollback spelling changes

--- qiskit_machine_learning/kernels/quantum_kernel.py
+++ qiskit_machine_learning/kernels/quantum_kernel.py
@@ -60,9 +60,9 @@ class QuantumKernel(TrainableKernel, BaseKernel):
 
     @deprecate_arguments("0.5.0", {"user_parameters": "training_parameters"})
     @deprecate_function(
-        msg="The QuantumKernel class has been superseded by the "
-        "'qiskit_machine_learning.kernels.FidelityQuantumKernel' and "
-        "'qiskit_machine_learning.kernels.TrainableFidelityQuantumKernel' classes. "
+        "The QuantumKernel class has been superseded by the "
+        "qiskit_machine_learning.kernels.FidelityQuantumKernel and "
+        "qiskit_machine_learning.kernels.TrainableFidelityQuantumKernel classes. "
         "This class will be deprecated in a future release and subsequently "
         "removed after that.",
         stacklevel=3,
