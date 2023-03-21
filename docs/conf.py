3a869ae3f671e1d180d1844839f409008e74a1f6
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-03-21 09:19:58 -0600

    Remove unnecessary `html_theme_path` option (#573)
    Co-authored-by: Anton Dekusar <62334182+adekusar-drl@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -88,13 +88,6 @@ nbsphinx_prolog += link_str + "{{ docname }}"
 
 # -- General configuration ---------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
@@ -183,18 +176,7 @@ modindex_common_prefix = ["qiskit_machine_learning."]
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#
 html_theme = "qiskit_sphinx_theme"
-
-html_theme_path = [".", qiskit_sphinx_theme.get_html_theme_path()]
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
 html_theme_options = {
     "logo_only": False,
     "display_version": True,
