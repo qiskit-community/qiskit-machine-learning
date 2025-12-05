68407c367d8453bba831edd835e11c8131915388
Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>
2025-12-05 20:28:28 +0000

    Add Py3.14

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Scientific/Engineering",
     ],
     keywords="qiskit sdk quantum machine learning ml",
