e0b9b9bc7c00a4b65cc9f00332d22dfbc54bc38e
AdithyaPhani <adithyaphani7@gmail.com>
2026-05-12 18:36:37 +0530

    cleans deprecated logic related behaviour and remove the automatic qubit padding

--- test/utils/test_adjust_num_qubits.py
+++ test/utils/test_adjust_num_qubits.py
@@ -10,7 +10,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-"""Tests for adjusting number of qubits in a feature map / ansatz."""
+"""Tests for derive_num_qubits_feature_map_ansatz."""
 
 from test import QiskitMachineLearningTestCase
 import itertools
@@ -22,7 +22,7 @@ from qiskit_machine_learning.utils import derive_num_qubits_feature_map_ansatz
 
 
 @ddt
-class TestAdjustNumQubits(QiskitMachineLearningTestCase):
+class TestDeriveNumQubits(QiskitMachineLearningTestCase):
     """Tests for the derive_num_qubits_feature_map_ansatz function."""
 
     def setUp(self) -> None:
@@ -35,19 +35,39 @@ class TestAdjustNumQubits(QiskitMachineLearningTestCase):
         }
 
     def test_all_none(self):
-        """Test when all parameters are ``None``."""
+        """All parameters None must raise."""
         self.assertRaises(
             QiskitMachineLearningError, derive_num_qubits_feature_map_ansatz, None, None, None
         )
 
-    def test_incompatible_feature_map_ansatz(self):
-        """Test when feature map and ansatz are incompatible."""
+    def test_mismatch_feature_map_ansatz(self):
+        """feature_map and ansatz with different qubit counts must raise."""
         self.assertRaises(
             QiskitMachineLearningError,
             derive_num_qubits_feature_map_ansatz,
             None,
             self.properties["z1"],
             self.properties["ra2"],
+        )
+
+    def test_mismatch_num_qubits_feature_map(self):
+        """num_qubits != feature_map.num_qubits must raise."""
+        self.assertRaises(
+            QiskitMachineLearningError,
+            derive_num_qubits_feature_map_ansatz,
+            2,
+            self.properties["z1"],
+            None,
+        )
+
+    def test_mismatch_num_qubits_ansatz(self):
+        """num_qubits != ansatz.num_qubits must raise."""
+        self.assertRaises(
+            QiskitMachineLearningError,
+            derive_num_qubits_feature_map_ansatz,
+            2,
+            None,
+            self.properties["ra1"],
         )
 
     @idata(
@@ -58,45 +78,28 @@ class TestAdjustNumQubits(QiskitMachineLearningTestCase):
     )
     @unpack
     def test_num_qubits_is_set(self, num_qubits, feature_map, ansatz):
-        """Test when the number of qubits is set."""
+        """When num_qubits is set and inputs agree."""
         feature_map = self.properties.get(feature_map)
         ansatz = self.properties.get(ansatz)
 
-        # derived objects
-        num_qubits_der, feature_map_der, ansatz_der = derive_num_qubits_feature_map_ansatz(
-            num_qubits, feature_map, ansatz
-        )
-        self.assertEqual(num_qubits_der, num_qubits)
-
-        self._test_feature_map(feature_map_der, feature_map, num_qubits)
-        self._test_ansatz(ansatz_der, num_qubits)
+        nq, fm, anz = derive_num_qubits_feature_map_ansatz(num_qubits, feature_map, ansatz)
+        self.assertEqual(nq, num_qubits)
+        self.assertEqual(fm.num_qubits, num_qubits)
+        self.assertEqual(anz.num_qubits, num_qubits)
+        self.assertIsInstance(fm, QuantumCircuit)
+        self.assertIsInstance(anz, QuantumCircuit)
 
     @idata([(None, "ra1"), (None, "ra2"), ("z1", None), ("z1", "ra1"), ("z2", None), ("z2", "ra2")])
     @unpack
     def test_num_qubits_isnot_set(self, feature_map, ansatz):
-        """Test when the number of qubits is not set."""
-        ansatz = self.properties.get(ansatz)
+        """When num_qubits is None and inputs determine the count."""
         feature_map = self.properties.get(feature_map)
+        ansatz = self.properties.get(ansatz)
+        expected = feature_map.num_qubits if feature_map is not None else ansatz.num_qubits
 
-        num_qubits_der, feature_map_der, ansatz_der = derive_num_qubits_feature_map_ansatz(
-            None, feature_map, ansatz
-        )
-        num_qubits = feature_map.num_qubits if feature_map is not None else ansatz.num_qubits
-
-        self.assertEqual(num_qubits_der, num_qubits)
-        self._test_feature_map(feature_map_der, feature_map, num_qubits)
-        self._test_ansatz(ansatz_der, num_qubits)
-
-    def _test_feature_map(self, feature_map_der, feature_map_org, num_qubits_expected):
-        self.assertIsNotNone(feature_map_der)
-        self.assertEqual(feature_map_der.num_qubits, num_qubits_expected)
-
-        if feature_map_org is None and num_qubits_expected == 1:
-            self.assertIsInstance(feature_map_der, QuantumCircuit)
-        if feature_map_org is None and num_qubits_expected == 2:
-            self.assertIsInstance(feature_map_der, QuantumCircuit)
-
-    def _test_ansatz(self, ansatz_der, num_qubits_expected):
-        self.assertIsNotNone(ansatz_der)
-        self.assertEqual(ansatz_der.num_qubits, num_qubits_expected)
-        self.assertIsInstance(ansatz_der, QuantumCircuit)
+        nq, fm, anz = derive_num_qubits_feature_map_ansatz(None, feature_map, ansatz)
+        self.assertEqual(nq, expected)
+        self.assertEqual(fm.num_qubits, expected)
+        self.assertEqual(anz.num_qubits, expected)
+        self.assertIsInstance(fm, QuantumCircuit)
+        self.assertIsInstance(anz, QuantumCircuit)
